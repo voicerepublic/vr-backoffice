9710c34557347c5feb71ac7e94e29dcc2affefde
Arvind kushwah <arvindkushwah9@gmail.com>
2020-10-31 15:04:26 +0530

    Update to rails6

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,26 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick
+
+  secret "7f65c8d4317cc27ae282bb064e9093beb2c255c5efac060685fa3957b0b020f4"
+
+  url_format "/media/:job/:name"
+
+  datastore :file,
+    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+    server_root: Rails.root.join('public')
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
+
+# Add model functionality
+ActiveSupport.on_load(:active_record) do
+  extend Dragonfly::Model
+  extend Dragonfly::Model::Validations
+end
