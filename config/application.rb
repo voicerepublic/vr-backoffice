b3aebaa1292f3d4ae48af9b7adac47071e0f2894
Arvind kushwah <arvindkushwah9@gmail.com>
2020-11-09 19:39:46 +0530

    Server srtart on productoin

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ Bundler.require(*Rails.groups)
 module VoicerepublicBackoffice
   class Application < Rails::Application
     config.load_defaults 6.0
-    # config.autoloader = :classic
+    config.autoloader = :classic
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
