2f6e8630feffaf4b82f7d4b92cf9a2f2522013ad
Arvind Kushwah <arvindkushwah9@gmail.com>
2021-02-19 10:58:14 +0530

    Add .env into share files

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :deploy_to, '/home/backend/app'
 
 # Default value for :linked_files is []
 set :linked_files, %w{ config/database.yml
-                       config/application.yml }
+                       config/.env }
 
 # Default value for linked_dirs is []
 #set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
